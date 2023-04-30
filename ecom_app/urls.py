68ee35f18404e897efbccbc3a0ca4257c565251f
Abhishek Sagar U <abhisheksagar513@gmail.com>
2023-04-30 21:05:15 +0530

    Adding Cache in some apis

--- ecom_app/urls.py
+++ ecom_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+""" Declaration of routes '/' and '/admin' """
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.urls')),
+    path('', include('products.urls')),
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
+
+
+""" Declaration of all routes comes under '/' """
 urlpatterns = [
-    path('',view=endpoints,name="All Endpoints"),
-    path('productpage',view=productpage,name="Product page"),
-    path('get_product/<str:product>',view=get_product,name="Get Product"),
-    path('list_products',view=list_products,name="List Products"),
-    path('create_product',view=create_product,name="Add Product"),
-    path('update_product',view=update_product,name="Update Product"),
-    path('delete_product',view=delete_product,name="Delete Product"),
+    path('', cache_page(60*60*2)(endpoints), name="All Endpoints"),
+    path('productpage', cache_page(60*60*2)(productpage), name="Product page"),
+    path('get_product/<str:product>', cache_page(60*60*2)(get_product), name="Get Product"),
+    path('list_products', cache_page(60*60*2)(list_products), name="List Products"),
+    path('create_product', view=create_product, name="Add Product"),
+    path('update_product', view=update_product, name="Update Product"),
+    path('delete_product', view=delete_product, name="Delete Product"),
 ]
