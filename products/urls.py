128c3651c45f0f7acf07d9530a0a4b158c2871ec
Abhishek Sagar U <abhisheksagar513@gmail.com>
2023-05-02 23:07:09 +0530

    removing caching from urls.py

--- products/urls.py
+++ products/urls.py
@@ -6,9 +6,11 @@ from django.views.decorators.cache import cache_page
 """ Declaration of all routes comes under '/' """
 urlpatterns = [
     path('', cache_page(60*60*2)(endpoints), name="All Endpoints"),
-    path('productpage', cache_page(60*60*2)(productpage), name="Product page"),
-    path('get_product/<str:product>', cache_page(60*60*2)(get_product), name="Get Product"),
-    path('list_products', cache_page(60*60*2)(list_products), name="List Products"),
+    # path('productpage', cache_page(60*60*2)(productpage), name="Product page"),
+    # path('list_products', cache_page(60*60*2)(list_products), name="List Products"),
+    path('productpage', productpage, name="Product page"),
+    path('list_products', list_products, name="List Products"),
+    path('get_product/<str:product>', get_product, name="Get Product"),
     path('create_product', view=create_product, name="Add Product"),
     path('update_product', view=update_product, name="Update Product"),
     path('delete_product', view=delete_product, name="Delete Product"),
